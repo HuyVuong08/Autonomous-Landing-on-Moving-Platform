489c5ec0d6680f5e510d3c2a70b8be39ec7c2491
HuyVuong08 <huy.vuongle123@gmail.com>
2021-09-19 22:54:07 +0700

    Code python to fly a drone in a square orbit

--- catkin_ws/src/parrot_ardrone/custom_teleop/src/teleop_twist_keyboard.py
+++ catkin_ws/src/parrot_ardrone/custom_teleop/src/teleop_twist_keyboard.py
@@ -34,109 +34,107 @@ CTRL-C to quit
 """
 
 moveBindings = {
-		'i':(1,0,0,0),
-		'o':(1,0,0,-1),
-		'j':(0,0,0,1),
-		'l':(0,0,0,-1),
-		'u':(1,0,0,1),
-		',':(-1,0,0,0),
-		'.':(-1,0,0,1),
-		'm':(-1,0,0,-1),
-		'O':(1,-1,0,0),
-		'I':(1,0,0,0),
-		'J':(0,1,0,0),
-		'L':(0,-1,0,0),
-		'U':(1,1,0,0),
-		'<':(-1,0,0,0),
-		'>':(-1,-1,0,0),
-		'M':(-1,1,0,0),
-		't':(0,0,1,0),
-		'b':(0,0,-1,0),
-	       }
+        'i':(1,0,0,0),
+        'o':(1,0,0,-1),
+        'j':(0,0,0,1),
+        'l':(0,0,0,-1),
+        'u':(1,0,0,1),
+        ',':(-1,0,0,0),
+        '.':(-1,0,0,1),
+        'm':(-1,0,0,-1),
+        'O':(1,-1,0,0),
+        'I':(1,0,0,0),
+        'J':(0,1,0,0),
+        'L':(0,-1,0,0),
+        'U':(1,1,0,0),
+        '<':(-1,0,0,0),
+        '>':(-1,-1,0,0),
+        'M':(-1,1,0,0),
+        't':(0,0,1,0),
+        'b':(0,0,-1,0),
+        }
 
 speedBindings={
-		'q':(1.1,1.1),
-		'z':(.9,.9),
-		'w':(1.1,1),
-		'x':(.9,1),
-		'e':(1,1.1),
-		'c':(1,.9),
-	      }
+        'q':(1.1,1.1),
+        'z':(.9,.9),
+        'w':(1.1,1),
+        'x':(.9,1),
+        'e':(1,1.1),
+        'c':(1,.9),
+        }
 
 def getKey():
-	tty.setraw(sys.stdin.fileno())
-	select.select([sys.stdin], [], [], 0)
-	key = sys.stdin.read(1)
-	termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-	return key
+    tty.setraw(sys.stdin.fileno())
+        select.select([sys.stdin], [], [], 0)
+        key = sys.stdin.read(1)
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+        return key
 
 speed = .5
 turn = 1
 
 def vels(speed,turn):
-	return "currently:\tspeed %s\tturn %s " % (speed,turn)
+    return "currently:\tspeed %s\tturn %s " % (speed,turn)
 
 if __name__=="__main__":
-    	settings = termios.tcgetattr(sys.stdin)
-	
-	pub = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
-	rospy.init_node('teleop_twist_keyboard')
-	pub2 = rospy.Publisher('drone/takeoff', Empty, queue_size = 1)
-	pub3 = rospy.Publisher('drone/land', Empty, queue_size = 1)
-	empty_msg = Empty()
+    settings = termios.tcgetattr(sys.stdin)
 
-	x = 0
-	y = 0
-	z = 0
-	th = 0
-	status = 0
+        pub = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
+        rospy.init_node('teleop_twist_keyboard')
+        pub2 = rospy.Publisher('drone/takeoff', Empty, queue_size = 1)
+        pub3 = rospy.Publisher('drone/land', Empty, queue_size = 1)
+        empty_msg = Empty()
 
-	try:
-		print msg
-		print vels(speed,turn)
-		while(1):
-			key = getKey()
-			if key in moveBindings.keys():
-				x = moveBindings[key][0]
-				y = moveBindings[key][1]
-				z = moveBindings[key][2]
-				th = moveBindings[key][3]
-			elif key in speedBindings.keys():
-				speed = speed * speedBindings[key][0]
-				turn = turn * speedBindings[key][1]
+        x = 0
+        y = 0
+        z = 0
+        th = 0
+        status = 0
 
-				print vels(speed,turn)
-				if (status == 14):
-					print msg
-				status = (status + 1) % 15
-				
-			elif key == '1':
-			    pub2.publish(empty_msg)
-			elif key == '2':
-			    pub3.publish(empty_msg)
-			
-			else:
-				x = 0
-				y = 0
-				z = 0
-				th = 0
-				if (key == '\x03'):
-					break
+        try:
+            print msg
+                print vels(speed,turn)
+                while(1):
+                    key = getKey()
+                        if key in moveBindings.keys():
+                            x = moveBindings[key][0]
+                                y = moveBindings[key][1]
+                                z = moveBindings[key][2]
+                                th = moveBindings[key][3]
+                        elif key in speedBindings.keys():
+                            speed = speed * speedBindings[key][0]
+                                turn = turn * speedBindings[key][1]
 
-			twist = Twist()
-			twist.linear.x = x*speed; twist.linear.y = y*speed; twist.linear.z = z*speed;
-			twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = th*turn
-			pub.publish(twist)
+                                print vels(speed,turn)
+                                if (status == 14):
+                                    print msg
+                                status = (status + 1) % 15
 
-	except:
-		print e
+                        elif key == '1':
+                            pub2.publish(empty_msg)
+                        elif key == '2':
+                            pub3.publish(empty_msg)
 
-	finally:
-		twist = Twist()
-		twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
-		twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
-		pub.publish(twist)
+                        else:
+                            x = 0
+                                y = 0
+                                z = 0
+                                th = 0
+                                if (key == '\x03'):
+                                    break
 
-    		termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+                        twist = Twist()
+                        twist.linear.x = x*speed; twist.linear.y = y*speed; twist.linear.z = z*speed;
+                        twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = th*turn
+                        pub.publish(twist)
 
+        except:
+            print e
 
+        finally:
+            twist = Twist()
+                twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
+                twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+                pub.publish(twist)
+
+                termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
