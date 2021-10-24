3a6324ef9bd358fe615954c4b1cb2158ff2ba766
HuyVuong08 <huy.vuongle123@gmail.com>
2021-10-25 02:09:19 +0700

    Add gps_common package and reindent xml xacro files

---
+++ pablo_uav_system_ws/src/thirdparty/gps_umd/gps_common/src/utm_odometry_node.cpp
@@ -0,0 +1,111 @@
+/*
+ * Translates sensor_msgs/NavSat{Fix,Status} into nav_msgs/Odometry using UTM
+ */
+
+#include <ros/ros.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <sensor_msgs/NavSatStatus.h>
+#include <sensor_msgs/NavSatFix.h>
+#include <gps_common/conversions.h>
+#include <nav_msgs/Odometry.h>
+
+using namespace gps_common;
+
+static ros::Publisher odom_pub;
+std::string frame_id, child_frame_id;
+double rot_cov;
+
+void conversion(double counts) {
+    std::ostringstream ss;
+    ss.precision(1);
+    ss << std::fixed << counts;
+    std::cout << ss.str() << " MeV";
+}
+
+void callback(const sensor_msgs::NavSatFixConstPtr& fix) {
+    if (fix->status.status == sensor_msgs::NavSatStatus::STATUS_NO_FIX) {
+        ROS_INFO("No fix.");
+        return;
+    }
+
+    if (fix->header.stamp == ros::Time(0)) {
+        return;
+    }
+
+    long double latitude, longitude;
+    double northing, easting;
+    std::string zone;
+    //latitude = fix->latitude;
+    //longitude = fix->longitude;
+    latitude = 0;
+    longitude = 0;
+
+    ROS_INFO("longitude = %Lf, latitude = %Lf", longitude, latitude);
+
+    LLtoUTM(latitude, longitude, northing, easting, zone);
+
+    ROS_INFO("easting = %f, northing = %f", easting, northing);
+
+    if (odom_pub) {
+        nav_msgs::Odometry odom;
+        odom.header.stamp = fix->header.stamp;
+
+        if (frame_id.empty())
+            odom.header.frame_id = fix->header.frame_id;
+        else
+            odom.header.frame_id = frame_id;
+
+        odom.child_frame_id = child_frame_id;
+
+        odom.pose.pose.position.x = easting;
+        odom.pose.pose.position.y = northing;
+        odom.pose.pose.position.z = fix->altitude;
+
+        odom.pose.pose.orientation.x = 0;
+        odom.pose.pose.orientation.y = 0;
+        odom.pose.pose.orientation.z = 0;
+        odom.pose.pose.orientation.w = 1;
+
+        ROS_INFO("x = %f, y = %f, z = %f", odom.pose.pose.position.x, odom.pose.pose.position.y, odom.pose.pose.position.z);
+
+        // Use ENU covariance to build XYZRPY covariance
+        boost::array<double, 36> covariance = {{
+            fix->position_covariance[0],
+            fix->position_covariance[1],
+            fix->position_covariance[2],
+            0, 0, 0,
+            fix->position_covariance[3],
+            fix->position_covariance[4],
+            fix->position_covariance[5],
+            0, 0, 0,
+            fix->position_covariance[6],
+            fix->position_covariance[7],
+            fix->position_covariance[8],
+            0, 0, 0,
+            0, 0, 0, rot_cov, 0, 0,
+            0, 0, 0, 0, rot_cov, 0,
+            0, 0, 0, 0, 0, rot_cov
+        }};
+
+        odom.pose.covariance = covariance;
+
+        odom_pub.publish(odom);
+    }
+}
+
+int main (int argc, char **argv) {
+    ros::init(argc, argv, "utm_odometry_node");
+    ros::NodeHandle node;
+    ros::NodeHandle priv_node("~");
+
+    priv_node.param<std::string>("frame_id", frame_id, "");
+    priv_node.param<std::string>("child_frame_id", child_frame_id, "");
+    priv_node.param<double>("rot_covariance", rot_cov, 99999.0);
+
+    odom_pub = node.advertise<nav_msgs::Odometry>("summit_xl/mavros/gps/odom", 10);
+
+    ros::Subscriber fix_sub = node.subscribe("summit_xl/mavros/gps/fix", 10, callback);
+
+    ros::spin();
+}
