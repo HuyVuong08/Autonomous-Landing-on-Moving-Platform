b6f70c693c5afc9eeb54a70461af032d4cd15758
HuyVuong08 <huy.vuongle123@gmail.com>
2022-04-30 21:28:56 +0700

    Remove build and devel folder

--- tello_madswamp_ver2_ws/build/uav_vision/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "cv_bridge;image_transport;roscpp;sensor_msgs;std_msgs;geometry_msgs;image_view;tf;std_srvs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "uav_vision"
-PROJECT_SPACE_DIR = "/home/tello1804/tello_madswamp_ver2_ws/install"
-PROJECT_VERSION = "0.0.0"
